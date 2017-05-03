34610b766cd55a9433e1ee45a27818d2d842e9e1
nightwind <nw4869@gmail.com>
2017-05-03 22:09:01 +0800

    在首页添加图片轮播和作品提交页面/按钮

--- app/main/views.py
+++ app/main/views.py
@@ -1,7 +1,13 @@
 from flask import render_template, request
+from flask_login import login_required
 from . import main
 
 
 @main.route('/')
 def index():
     return render_template('index.html')
+
+@login_required
+@main.route('/upload')
+def upload():
+    return render_template('upload.html')
